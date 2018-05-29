fc8c61fe91519ed4fb043b2074a195744691f89b
Morten Sickel <morten@sickel.net>
2018-05-29 13:32:34 +0200

    Javascript to enable and distable button favicon

--- static/process.js
+++ static/process.js
@@ -1,1 +1,25 @@
-alert("Hullu!");
+
+var fatalerrors=false;
+
+window.onload = function(evt){
+    var warns = document.getElementsByClassName("warncheck");
+    for(var i = 0; i < warns.length; i++)
+    {
+       warns[i].onclick=checkwarn;
+    }
+    warnings=!!document.getElementById("Warningheader");
+    fatalerrors=!!document.getElementById("Errorheader")
+    if (warnings || fatalerrors){
+        document.getElementById("importdata").disabled = true;
+    }
+}
+
+checkwarn=function(evt){
+    enabled=true
+    var warns = document.getElementsByClassName("warncheck");
+    for(var i = 0; i < warns.length; i++)
+    {
+       enabled=enabled && warns[i].checked
+    }
+    document.getElementById("importdata").disabled = !enabled || fatalerrors;
+}
